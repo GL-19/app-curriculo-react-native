9182a0e89568994d98b5db2298e5826711a6ae3f
GL-19 <loiolagl19@gmail.com>
2021-11-04 12:18:08 -0300

    Aplicação finalizada

--- src/App.js
+++ src/App.js
@@ -1,132 +1,105 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
-  Alert,
-} from 'react-native';
-import Icon from 'react-native-vector-icons/Feather';
-
-import foto from './assets/foto.jpeg';
+import React from "react";
+import { View, Text, StyleSheet, Image, TouchableOpacity, Alert } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import { Card } from "./components/Card";
+import photo from "./assets/foto.jpeg";
 
 export function App() {
-  function handlePress(redeSocial) {
-    switch (redeSocial) {
-      case 'linkedin':
-        Alert.alert('Meu linkedin', 'https://linkedin.com');
-        break;
-      case 'facebook':
-        Alert.alert('Meu facebook', 'https://facebook.com');
-        break;
-      case 'github':
-        Alert.alert('Meu github', 'https://github.com');
-        break;
-    }
-  }
-  return (
-    <>
-      <View style={style.page}>
-        <View style={style.container_cabecalho}>
-          <Image source={foto} style={style.photo} />
-          <Text style={style.nome}>ANA MARIA</Text>
-          <Text style={style.funcao}>Desenvolvedora Mobile</Text>
-          <View style={style.redes_sociais}>
-            <TouchableOpacity onPress={() => handlePress('github')}>
-              <Icon name="github" size={30} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handlePress('facebook')}>
-              <Icon name="facebook" size={30} />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => handlePress('linkedin')}>
-              <Icon name="linkedin" size={30} />
-            </TouchableOpacity>
-          </View>
-        </View>
+	function handlePress(socialNetwork) {
+		switch (socialNetwork) {
+			case "linkedin":
+				Alert.alert("Meu linkedin", "https://linkedin.com");
+				break;
+			case "facebook":
+				Alert.alert("Meu facebook", "https://facebook.com");
+				break;
+			case "github":
+				Alert.alert("Meu github", "https://github.com");
+				break;
+		}
+	}
 
-        <View style={style.card_container}>
-          <View style={style.card}>
-            <View style={style.card_header}>
-              <Text>Experiência Profissional</Text>
-            </View>
-            <View style={style.card_content}>
-              <Text style={style.card_content_text}>Disneylandia</Text>
-              <Text style={style.card_content_text}>Lalala</Text>
-              <Text style={style.card_content_text}>Outro lugar</Text>
-            </View>
-          </View>
-        </View>
+	return (
+		<>
+			<View style={style.page}>
+				<View style={style.headerContainer}>
+					<Image source={photo} style={style.photo} />
+					<Text style={style.name}>ANA MARIA</Text>
+					<Text style={style.job}>Desenvolvedora Mobile</Text>
+					<View style={style.socialNetworks}>
+						<TouchableOpacity onPress={() => handlePress("github")}>
+							<Icon name="github" size={30} />
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() => handlePress("facebook")}>
+							<Icon name="facebook" size={30} />
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() => handlePress("linkedin")}>
+							<Icon name="linkedin" size={30} />
+						</TouchableOpacity>
+					</View>
+				</View>
+				<Card
+					title="Experiência Profissional"
+					content={["Disneylandia", "Lalala", "Outra empresa"]}
+				/>
 
-        <View style={style.card_container}>
-          <View style={style.card}>
-            <View style={style.card_header}>
-              <Text>Formação Academica</Text>
-            </View>
-            <View style={style.card_content}>
-              <Text style={style.card_content_text}>
-                Ciências da Computação - UFMG
-              </Text>
-            </View>
-          </View>
-        </View>
-      </View>
-    </>
-  );
+				<Card title="Formação Academica" content={["Ciências da Computação - UFMG"]} />
+			</View>
+		</>
+	);
 }
 
 const style = StyleSheet.create({
-  page: {
-    backgroundColor: '#E7E7E7',
-    flex: 1,
-    alignItems: 'center',
-  },
-  container_cabecalho: {
-    alignItems: 'center',
-    marginTop: 25,
-    flex: 5,
-  },
-  photo: {
-    width: 180,
-    height: 180,
-    borderRadius: 125,
-  },
-  nome: {
-    fontSize: 26,
-    fontWeight: 'bold',
-    marginTop: 10,
-  },
-  funcao: {
-    color: '#939393',
-    marginBottom: 10,
-  },
-  redes_sociais: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '75%',
-  },
-  card_container: {
-    width: '100%',
-    marginTop: 20,
-    flex: 2,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-  },
-  card: {
-    width: '50%',
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#939393',
-    padding: 10,
-    backgroundColor: '#fff',
-  },
-
-  card_header: {},
-  card_content: {
-    marginTop: 10,
-  },
-  card_content_text: {
-    color: '#939393',
-    marginBottom: 5,
-  },
+	page: {
+		backgroundColor: "#E7E7E7",
+		flex: 1,
+		alignItems: "center",
+	},
+	headerContainer: {
+		alignItems: "center",
+		marginTop: 25,
+		flex: 5,
+	},
+	photo: {
+		width: 180,
+		height: 180,
+		borderRadius: 125,
+	},
+	name: {
+		fontSize: 26,
+		fontWeight: "bold",
+		marginTop: 10,
+	},
+	job: {
+		color: "#939393",
+		marginBottom: 10,
+	},
+	socialNetworks: {
+		flexDirection: "row",
+		justifyContent: "space-evenly",
+		width: "75%",
+	},
+	cardContainer: {
+		width: "100%",
+		marginTop: 20,
+		flex: 2,
+		justifyContent: "flex-start",
+		alignItems: "center",
+	},
+	card: {
+		width: "50%",
+		borderRadius: 5,
+		borderWidth: 1,
+		borderColor: "#939393",
+		padding: 10,
+		backgroundColor: "#fff",
+	},
+	cardHeader: {},
+	cardContent: {
+		marginTop: 10,
+	},
+	cardContentText: {
+		color: "#939393",
+		marginBottom: 5,
+	},
 });
